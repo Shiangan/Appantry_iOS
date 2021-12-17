2fda4b1a388848d69ac85731ea19c85c7bc8b4dc
Jan <jan.locher@hotmail.ch>
2021-12-17 11:42:19 +0100

    Initial Commit

---
+++ Appantry_iOS/Appantry_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appantry_iOSApp.swift
+//  Appantry_iOS
+//
+//  Created by Jan Locher on 17.12.21.
+//
+
+import SwiftUI
+
+@main
+struct Appantry_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
